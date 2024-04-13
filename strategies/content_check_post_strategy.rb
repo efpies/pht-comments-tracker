695fa788bbe44c3ec9e72dc62e9c7922415097ac
Nicholas Yasnov <nickolay.yasnov@qoden.com>
2024-04-13 16:41:34 +0200

    Improve code readability

--- strategies/content_check_post_strategy.rb
+++ strategies/content_check_post_strategy.rb
@@ -15,14 +15,13 @@ class ContentCheckPostStrategy
     id = post.post_id.to_i
     old_count = post.comments_count
 
-    post_content = @post_service.get_flow_post id
-
-    new_comments_count = post_content['comments_count']
-    new_comments_count = 'н/к' if new_comments_count.zero? && post_content['disable_comments']
-
     post_url = "https://#{@content_uri.host}/#/publicate/#{id}?ordering=-created_at"
 
-    unless new_comments_count.zero?
+    post_content = @post_service.get_flow_post id
+    new_comments_count = post_content['comments_count']
+    if new_comments_count.zero?
+      new_comments_count = 'н/к' if post_content['disable_comments']
+    else
       last_comment_id = @pht_client.fetch_last_top_level_comment_id(id)
       post_url += "&comment_target_id=#{last_comment_id}" unless last_comment_id.nil?
     end
